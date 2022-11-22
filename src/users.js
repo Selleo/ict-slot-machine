3d23dd16596a32907be04806acaa834621e1fada
Dawid Holisz <dawid.holisz@gmail.com>
2022-11-22 12:25:00 +0100

    change mentions to slackIds

--- src/users.js
+++ src/users.js
@@ -1,367 +1,363 @@
 export const USERS = {
   5501512322897: {
-    mention: 'czana',
+    mention: 'U6TVDKGJ2',
     email: 't.czana@selleo.com',
-    admin: true
+    admin: true,
   },
   4402038631408: {
-    mention: 'bart',
-    email: 'b.wojtowicz@selleo.com'
+    mention: 'U6S0VDENQ',
+    email: 'b.wojtowicz@selleo.com',
   },
   5501412812678: {
-    mention: 'Piotr',
-    email: 'p.motak@selleo.com'
+    mention: 'UKVRV75D1',
+    email: 'p.motak@selleo.com',
   },
   4402038648119: {
-    mention: 'm.polakowski',
+    mention: 'U6S0A54MP',
     email: 'm.polakowski@selleo.com',
-    slackUserId: 'U6S0A54MP'
   },
   5501557468263: {
-    mention: 'apawlicka',
-    email: 'a.pawlicka@selleo.com'
+    mention: 'U6SKMNB3P',
+    email: 'a.pawlicka@selleo.com',
   },
   5501579525414: {
-    mention: 'kijek',
+    mention: 'U9JJY5F53',
     email: 'l.lapczyk@selleo.com',
-    slackUserId: 'U9JJY5F53'
   },
   4402143914145: {
-    mention: 'kovson',
+    mention: 'U6SGZ5G1Y',
     email: 'k.gren@selleo.com',
-    slackUserId: 'U6SGZ5G1Y'
   },
   5501567041255: {
-    mention: 'stevo',
-    email: 'b.kosmowski@selleo.com'
+    mention: 'U6XFE6GJ3',
+    email: 'b.kosmowski@selleo.com',
   },
   4402143917759: {
     mention: 'U02SUTZ4LN5',
-    email: 'a.konopka@selleo.com'
+    email: 'a.konopka@selleo.com',
   },
   4402038639377: {
-    mention: 'Justyna',
-    email: 'j.jakubiec@selleo.com'
+    mention: 'U02H6D5MT09',
+    email: 'j.jakubiec@selleo.com',
   },
   5501550893967: {
-    mention: 'mikoscz',
-    email: 'm.staskiewicz@selleo.com'
+    mention: 'U6TGCDNKH',
+    email: 'm.staskiewicz@selleo.com',
   },
   18693438713984: {
-    mention: 'KasiaL',
-    email: 'k.legien@selleo.com'
+    mention: 'U01QJ4ECGGG',
+    email: 'k.legien@selleo.com',
   },
   4402128343148: {
-    mention: 'matts',
-    email: 'm.skrobis@selleo.com'
+    mention: 'U6SF1ER28',
+    email: 'm.skrobis@selleo.com',
   },
   5501493914965: {
-    mention: 'renerte',
-    email: 'd.suchy@selleo.com'
+    mention: 'U6SCY948Y',
+    email: 'd.suchy@selleo.com',
   },
   4402148028600: {
-    mention: 'jarcos',
-    email: 'j.kisiolek@selleo.com'
+    mention: 'U6RT07KU1',
+    email: 'j.kisiolek@selleo.com',
   },
   5498835649833: {
-    mention: 'robson',
-    email: 'r.harezlak@selleo.com'
+    mention: 'U6TCN69ML',
+    email: 'r.harezlak@selleo.com',
   },
   4402037015153: {
-    mention: 'dawid',
-    email: 'd.poslinski@selleo.com'
+    mention: 'U6RTDMLAV',
+    email: 'd.poslinski@selleo.com',
   },
   5501574153520: {
-    mention: 'tgn',
-    email: 't.noworyta@selleo.com'
+    mention: 'U6SF5DRMJ',
+    email: 't.noworyta@selleo.com',
   },
   5501512316854: {
-    mention: 'wojtek',
-    email: 'w.wrona@selleo.com'
+    mention: 'U6SB53BMZ',
+    email: 'w.wrona@selleo.com',
   },
   18693305535122: {
-    mention: 'irek',
-    email: 'i.skrobis@selleo.com'
+    mention: 'U6SD9UW90',
+    email: 'i.skrobis@selleo.com',
   },
   5501555273892: {
-    mention: 'wieczorek',
-    email: 'm.wieczorek@selleo.com'
+    mention: 'U6SHM0CMA',
+    email: 'm.wieczorek@selleo.com',
   },
   5501520053451: {
-    mention: 'kantyka',
-    email: 'm.kantyka@selleo.com'
+    mention: 'U9HR4GFRV',
+    email: 'm.kantyka@selleo.com',
   },
   5501566352248: {
-    mention: 'Grzegorz Zaręba',
-    email: 'g.zareba@selleo.com'
+    mention: 'U9UFLPUKZ',
+    email: 'g.zareba@selleo.com',
   },
   5501566395940: {
-    mention: 'arekm',
-    email: 'a.machalica@selleo.com'
+    mention: 'U6TSVQZAA',
+    email: 'a.machalica@selleo.com',
   },
   5498835641930: {
-    mention: 'janek',
-    email: 'a.janik@selleo.com'
+    mention: 'U6SKCJNCV',
+    email: 'a.janik@selleo.com',
   },
   4402052980935: {
-    mention: 'pien',
-    email: 'd.pienczykowski@selleo.com'
+    mention: 'U6UM94QEB',
+    email: 'd.pienczykowski@selleo.com',
   },
   5501520047650: {
-    mention: 'b.boruta',
+    mention: 'UA054LMFU',
     email: 'b.boruta@selleo.com',
-    slackUserId: 'UA054LMFU'
   },
   9899584476032: {
-    mention: 'tommy',
-    email: 't.dudzik@selleo.com'
+    mention: 'U6T5AG9B5',
+    email: 't.dudzik@selleo.com',
   },
   4402038678975: {
-    mention: 'bmaka',
-    email: 'b.maka@selleo.com'
+    mention: 'U6XMDUJV9',
+    email: 'b.maka@selleo.com',
   },
   4402148018063: {
     mention: 'U031677E0NR',
-    email: 'k.poziomek@selleo.com'
+    email: 'k.poziomek@selleo.com',
   },
   4402148020376: {
     mention: 'U01MGKFRDFH',
-    email: 'n.witek@selleo.com'
+    email: 'n.witek@selleo.com',
   },
   5501522899144: {
     mention: 'U02J3EBAGRH',
-    email: 'm.suplat@selleo.com'
+    email: 'm.suplat@selleo.com',
   },
   4402038631671: {
-    mention: 'dadesigner',
-    email: 'l.siwek@selleo.com'
+    mention: 'U6TSZ3Y06',
+    email: 'l.siwek@selleo.com',
   },
   5501579533248: {
     mention: 'U032XAEHN3V',
-    email: 's.krasny@selleo.com'
+    email: 's.krasny@selleo.com',
   },
   4402038639638: {
-    mention: 'Kasia',
-    email: 'k.babinska@selleo.com'
+    mention: 'U93RRV21G',
+    email: 'k.babinska@selleo.com',
   },
   4402036975254: {
-    mention: 'dominikduda',
-    email: 'd.duda@selleo.com'
+    mention: 'U6TH8A11V',
+    email: 'd.duda@selleo.com',
   },
   5501568626383: {
-    mention: 'bob',
-    email: 'w.bozek@selleo.com'
+    mention: 'U6S06MCRF',
+    email: 'w.bozek@selleo.com',
   },
   5501567016069: {
-    mention: 'tromik',
-    email: 't.romik@selleo.com'
+    mention: 'U6RPJ91G8',
+    email: 't.romik@selleo.com',
   },
   5497845585440: {
-    mention: 'Maciej',
-    email: 'm.hubert@selleo.com'
+    mention: 'UTF0MSJ4D',
+    email: 'm.hubert@selleo.com',
   },
   5501425353830: {
     mention: 'U7XJUDWFK',
-    email: 'p.chalecki@selleo.com'
+    email: 'p.chalecki@selleo.com',
   },
   48382517907206: {
-    mention: 'grduch',
-    email: 'g.rduch@selleo.com'
+    mention: 'U6VMZU3CY',
+    email: 'g.rduch@selleo.com',
   },
   5501522904124: {
-    mention: 'Anna Łękawa',
-    email: 'a.lekawa@selleo.com'
+    mention: 'U830E8CP6',
+    email: 'a.lekawa@selleo.com',
   },
   18693294387349: {
-    mention: 'dariusz_wylon',
-    email: 'd.wylon@selleo.com'
+    mention: 'U6WDL1AF7',
+    email: 'd.wylon@selleo.com',
   },
   5501520079015: {
-    mention: 'tb',
-    email: 't.bak@selleo.com'
+    mention: 'U713VQDS6',
+    email: 't.bak@selleo.com',
   },
   5501524317085: {
-    mention: 'tomasz-zwak',
-    email: 't.zwak@selleo.com'
+    mention: 'U02NV8P87C7',
+    email: 't.zwak@selleo.com',
   },
   8798880286667: {
-    mention: 'm.cholewka',
-    email: 'm.cholewka@selleo.com'
+    mention: 'UGPAK7MNW',
+    email: 'm.cholewka@selleo.com',
   },
   8798880124543: {
-    mention: 'Ciotka',
-    email: 'a.nikiel@selleo.com'
+    mention: 'UGZED1LKA',
+    email: 'a.nikiel@selleo.com',
   },
   8798909519385: {
-    mention: 'm.ryczek',
-    email: 'm.ryczek@selleo.com'
+    mention: 'U02BA1G1EDR',
+    email: 'm.ryczek@selleo.com',
   },
   8798880304899: {
     mention: 'U03P7KA3JF7',
-    email: 't.bilka@selleo.com'
+    email: 't.bilka@selleo.com',
   },
   8798954514628: {
-    mention: 'p.peleszynski',
-    email: 'p.peleszynski@selleo.com'
+    mention: 'UKLUQJHME',
+    email: 'p.peleszynski@selleo.com',
   },
   18693305557754: {
-    mention: 'd.holisz',
-    email: 'd.holisz@selleo.com'
+    mention: 'UL0U1817A',
+    email: 'd.holisz@selleo.com',
   },
   8799048379061: {
-    mention: 'Piotr',
-    email: 'p.motak@selleo.com'
+    mention: 'UKVRV75D1',
+    email: 'p.motak@selleo.com',
   },
   5501532454452: {
     mention: 'U6T4LREPR',
-    email: 's.kieloch@selleo.com'
+    email: 's.kieloch@selleo.com',
   },
   8798958669089: {
     mention: 'UL8FYLFMW',
-    email: 'a.turek@selleo.com'
+    email: 'a.turek@selleo.com',
   },
   9899584484256: {
     mention: 'UML5E8YBD',
-    email: 'd.loranc@selleo.com'
+    email: 'd.loranc@selleo.com',
   },
   9899621396540: {
     mention: 'UNP7KTT8S',
-    email: 'e.wydra@selleo.com'
+    email: 'e.wydra@selleo.com',
   },
   9899621294255: {
-    mention: 'Adam',
-    email: 'a.wozny@selleo.com'
+    mention: 'UQ6M2CNPQ',
+    email: 'a.wozny@selleo.com',
   },
   47280065230428: {
     mention: 'UTZVBH9C4',
-    email: 'b.fryczkowska@selleo.com'
+    email: 'b.fryczkowska@selleo.com',
   },
   47280033467905: {
-    mention: 'Tomasz Prochowicz',
-    email: 't.prochowicz@selleo.com'
+    mention: 'U01NUKPHBHR',
+    email: 't.prochowicz@selleo.com',
   },
   9899637361389: {
-    mention: 'murmeltier',
-    email: 'm.zupa@selleo.com'
+    mention: 'U02PGPCD0G1',
+    email: 'm.zupa@selleo.com',
   },
   47280063507866: {
-    mention: 'Asia H',
-    email: 'a.honkisz@selleo.com'
+    mention: 'UTNUWJ18T',
+    email: 'a.honkisz@selleo.com',
   },
   47280031650744: {
     mention: 'U01NA1S34EM',
-    email: 'm.firszt@selleo.com'
+    email: 'm.firszt@selleo.com',
   },
   47280088158136: {
-    mention: 'david',
-    email: 'd.kachel@selleo.com'
+    mention: 'U01TR3A9EN4',
+    email: 'd.kachel@selleo.com',
   },
   5501555271089: {
-    mention: 'a.surma',
-    email: 'a.surma@selleo.com'
+    mention: 'UU2KEPEFQ',
+    email: 'a.surma@selleo.com',
   },
   8799049564353: {
     mention: 'U01TBH6GC4B',
-    email: 'k.boruta@selleo.com'
+    email: 'k.boruta@selleo.com',
   },
   5501561394339: {
-    mention: 'Mateusz Matejko',
-    email: 'm.matejko@selleo.com'
+    mention: 'U01T4HFNU14',
+    email: 'm.matejko@selleo.com',
   },
   8798880303622: {
-    mention: 'marzena',
-    email: 'm.orawczak@selleo.com'
+    mention: 'U029EJ7SU3Z',
+    email: 'm.orawczak@selleo.com',
   },
   9899584482495: {
-    mention: 'tomeQAtomek',
-    email: 't.jakobiec@selleo.com'
+    mention: 'U01B85NKXQU',
+    email: 't.szyrner@selleo.com,',
   },
   18693326756786: {
-    mention: 'Ania Pawlik',
-    email: 'a.pawlik@selleo.com'
+    mention: 'UREDQF0D7',
+    email: 'a.pawlik@selleo.com',
   },
   4402052972185: {
-    mention: 'Mateo',
-    email: 'm.pekala@selleo.com'
+    mention: 'U01DTL37NV9',
+    email: 'm.pekala@selleo.com',
   },
   5501523650653: {
-    mention: 'synion',
-    email: 's.lach@selleo.com'
+    mention: 'U6Y0ANQPR',
+    email: 's.lach@selleo.com',
   },
   18693323787281: {
-    mention: 'matro',
-    email: 'm.troszczynski@selleo.com'
+    mention: 'U02EW1PBL1E',
+    email: 'm.troszczynski@selleo.com',
   },
   18693305531495: {
-    mention: 'Dorota',
-    email: 'd.lukojc@selleo.com'
+    mention: 'U02J09TPFAL',
+    email: 'd.lukojc@selleo.com',
   },
   18693298344821: {
-    mention: 'JWilk',
-    email: 'j.wilk@selleo.com'
+    mention: 'U02LJT01Y0Y',
+    email: 'j.wilk@selleo.com',
   },
   18693617201061: {
-    mention: 'milosz.zajonc',
-    email: 'm.zajonc@selleo.com'
+    mention: 'U02LJT05LTA',
+    email: 'm.zajonc@selleo.com',
   },
   18693298349889: {
     mention: 'U02LJT07GE4',
-    email: 'j.ciupka@selleo.com'
+    email: 'j.ciupka@selleo.com',
   },
   18693617123977: {
     mention: 'U02KV75P3MY',
-    email: 'm.dziubkorczynska@selleo.com'
+    email: 'm.dziubkorczynska@selleo.com',
   },
   18693617206735: {
-    mention: 'd.obajtek',
-    email: 'd.obajtek@selleo.com'
+    mention: 'U02M8530STX',
+    email: 'd.obajtek@selleo.com',
   },
   18693617118566: {
-    mention: 'Anna Bala',
-    email: 'a.bala@selleo.com'
+    mention: 'U02MD3FS9U4',
+    email: 'a.bala@selleo.com',
   },
   18693305554124: {
-    mention: 'Adrian Pilch',
-    email: 'a.pilch@selleo.com'
+    mention: 'U02MD3G2GBW',
+    email: 'a.pilch@selleo.com',
   },
   18693438731203: {
     mention: 'U03HCL9CL1M',
-    email: 'i.dolotov@selleo.com'
+    email: 'i.dolotov@selleo.com',
   },
   18693617206988: {
     mention: 'U03HT8XQDJN',
-    email: 'd.grygierczyk@selleo.com'
+    email: 'd.grygierczyk@selleo.com',
   },
   4402052951115: {
     mention: 'U03RAB65N3H',
-    email: 'm.nowicka@selleo.com'
+    email: 'm.nowicka@selleo.com',
   },
   18693438708651: {
     mention: 'U0352E4J5SP',
-    email: 'a.jachymek@selleo.com'
+    email: 'a.jachymek@selleo.com',
   },
   8798961148022: {
     mention: 'U0229MYJUJJ',
-    email: 't.kielar@selleo.com'
+    email: 't.kielar@selleo.com',
   },
   18693617118048: {
     mention: 'U042WVD4T0T',
-    email: 'k.mentel@selleo.com'
+    email: 'k.mentel@selleo.com',
   },
   5501524328654: {
     mention: 'U02KXDH1ZPE',
-    email: 'm.klejdysz@selleo.com'
+    email: 'm.klejdysz@selleo.com',
   },
   47280063141906: {
     mention: 'U02LJT05LTA',
-    email: 'm.zajonc@selleo.com'
+    email: 'm.zajonc@selleo.com',
   },
   5501524337388: {
     mention: 'U0496KMULQL',
-    email: 'k.zontek@selleo.com'
+    email: 'k.zontek@selleo.com',
   },
 }
 
-export default id => {
+export default (id) => {
   return USERS[id]
-}
+};
