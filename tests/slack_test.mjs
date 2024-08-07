c71fbe5868e58979c1c9764ddf73b7c895056fe0
Dawid Loranc <dawidloranc@gmail.com>
2024-08-07 14:18:12 +0200

    Fix Slack test

--- tests/slack_test.mjs
+++ tests/slack_test.mjs
@@ -1,5 +1,5 @@
 import Slack from '../src/slack.js'
 
-const slack = new Slack(process.env.SLACK_LOG_WEBHOOK)
+const slack = new Slack(process.env.SLACK_LOG_WEBHOOK, process.env.SLACK_LOG_WEBHOOK)
 
 slack.log('test Slacka')
