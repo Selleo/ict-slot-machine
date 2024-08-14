d4bf18eecf570caa3378710b3f45d29224c0206a
Dawid Loranc <dawidloranc@gmail.com>
2024-08-14 20:38:30 +0200

    Retrieve all users to bypass the rate limit issue

--- src/api.js
+++ src/api.js
@@ -34,6 +34,36 @@ class Api {
     }
   }
 
+  static async getUsers(retryCount = 0) {
+    try {
+      const response = await fetch(`${process.env.URL}/rfids`, {
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": Api.authToken,
+        }
+      });
+
+      if (response.ok) {
+        return await response.json();
+      } else if (retryCount < 3) {
+        console.error(`Authentication failed. Retrying login in ${RETRY_DELAY}ms...`);
+        try {
+          await Api.delay();
+          await Api.login();
+          return Api.getUsers(retryCount + 1);
+        } catch (loginError) {
+          console.error('Retry login failed:', loginError.message);
+          throw loginError;
+        }
+      } else {
+        throw new Error('Failed to authenticate.');
+      }
+    } catch (error) {
+      console.error('Failed to get all RFID data:', error.message);
+      console.error(error);
+    }
+  }
+
   static async getUser(rfidKey, retryCount = 0) {
     try {
       const response = await fetch(`${process.env.URL}/rfids/${rfidKey}`, {
