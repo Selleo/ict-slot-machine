028cc42a2f1ed6b98f77213da29ad4832ae03532
Dawid Loranc <dawidloranc@gmail.com>
2024-08-29 11:31:14 +0200

    Add test

--- tests/slack_test.mjs
+++ tests/slack_test.mjs
@@ -2,4 +2,4 @@ import Slack from '../src/slack.js'
 
 const slack = new Slack(process.env.SLACK_LOG_WEBHOOK, process.env.SLACK_LOG_WEBHOOK)
 
-slack.log('test Slacka')
+slack.log('<@UML5E8YBD> mention')
